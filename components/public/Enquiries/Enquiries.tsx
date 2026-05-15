54da08895de67b6155ed4b7efa4478f96e29c8d2
quarshie133 <wrightsprince@gmail.com>
2026-05-15 13:20:51 +0000

    changed wording

--- components/public/Enquiries/Enquiries.tsx
+++ components/public/Enquiries/Enquiries.tsx
@@ -71,7 +71,7 @@ export default function Enquiries() {
             rel="noopener noreferrer"
             className={styles['cta-btn']}
           >
-            Click to Buy &amp; Register
+            Buy your Forms Here
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" style={{ marginLeft: '0.5rem' }}>
               <path d="M5 12h14M13 6l6 6-6 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
